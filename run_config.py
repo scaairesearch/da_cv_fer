9b120bdd9dc325bf96afe8d650d342ccf2cb52cd
Peeyush Singhal <peeyushsinghal@users.noreply.github.com>
2023-12-20 16:25:15 +0530

    Update run_config.py
    different configs for Colab and local

--- run_config.py
+++ run_config.py
@@ -2,11 +2,20 @@ from pathlib import Path
 import torch
 import torch.optim as optim
 import torch.nn as nn
+import os
 
 class RunConfig:
     def __init__(self) -> None:
-        self.EPOCHS = 2
-        self.NUM_EPOCHS = 2
+        IN_COLAB = False
+        if os.path.exists('/content'): # check for google colab
+            IN_COLAB = True
+
+        if IN_COLAB:
+            self.EPOCHS = 10
+            self.NUM_EPOCHS = 10
+        else:
+            self.EPOCHS = 1
+            self.NUM_EPOCHS = 1
         # self.optimizer = None
         self.lr_strategy = None
         self.inital_lr = 0.001
