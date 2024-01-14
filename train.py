3e882068b9d55358bf44e5dc56797cada9dd24f6
Peeyush Singhal <peeyushsinghal@users.noreply.github.com>
2024-01-14 11:48:08 +0530

    training functions

---
+++ train.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm # for beautiful model training updates
+
+# initializating weights using xavier normal
+def weights_init(m):
+    if isinstance(m, nn.Conv2d):
+      # nn.init.xavier_normal_(m.weight)
+      pass # not initialising Conv layers as they are pretrained
+    elif isinstance(m, nn.Linear):
+      nn.init.xavier_normal_(m.weight) # Linear layers are added on
+      nn.init.zeros_(m.bias)
+
+#@title Typical Training Function with Learning rate
+
+def train_model(model,device, data_loader, optimizer, epoch,
+                class_loss_function = nn.CrossEntropyLoss() ):
+    print(f'epoch = {epoch+1}...in training function')
+
+    model.train() # setting model to training
+    model.to(device)
+
+    pbar = tqdm(data_loader) # putting the dataloader in progress bar
+
+    processed = 0 # to understand accuracy, used in denominator
+    epoch_loss = 0.0
+
+    for batch_idx, batch in enumerate(pbar):
+        images, labels = batch[0].to(device), batch[1].to(device) # batch of images and labels
+
+        optimizer.zero_grad() # setting gradients to zero to avoid accumulation
+
+        class_output, _ , _ = model(images) # forward pass, output = {class, domain, feature}
+
+        class_loss = class_loss_function(class_output,labels) # Calculate Loss
+        epoch_loss += class_loss.item()
+
+        class_loss.backward() #backpropogation, creating gradients
+
+        optimizer.step() # updating the params
+
+        processed += len(images)
+        curr_lr = optimizer.param_groups[0]['lr']
+
+        pbar.set_description(desc= f'Loss={class_loss.item()} Batch_id={batch_idx+1} Epoch Average loss={epoch_loss/processed:0.8f} LR={curr_lr:0.6f}')
+
+    return float("{:.8f}".format(epoch_loss/processed)),  curr_lr
+
+def get_correct_predictions(prediction, labels):
+    """
+    Function to return total number of correct predictions
+    :param prediction: Model predictions on a given sample of data
+    :param labels: Correct labels of a given sample of data
+    :return: Number of correct predictions
+    """
+    return prediction.argmax(dim=1).eq(labels.argmax(dim=1)).sum().item()
+
+
+#@title Typical Test Function
+def test_model(model, device, data_loader, mode= 'test',
+               class_loss_function = nn.CrossEntropyLoss() ):
+    model.eval() # evaluation mode
+
+    # variables to track loss and accuracy
+    test_loss = 0.0
+    correct = 0
+
+    # Disable gradient updation
+    with torch.no_grad():
+        # Iterate over each batch and fetch images and labels from the batch
+        for batch_idx, batch in enumerate(data_loader):
+            images, labels = batch[0].to(device), batch[1].to(device)
+
+            class_output, _ , _ = model(images) # forward pass, output = {class, domain, feature}
+            class_loss = class_loss_function(class_output,labels) # Calculate Loss
+
+            test_loss += class_loss.item() # sum up batch loss
+
+            # Sum up batch correct predictions
+            correct += get_correct_predictions(class_output, labels)
+
+    # Calculate test loss for a epoch
+    test_loss /= len(data_loader.dataset)
+
+    print('Test / Val: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(test_loss, correct, len(data_loader.dataset),
+        100. * correct / len(data_loader.dataset)))
+
+
+    return correct, test_loss
