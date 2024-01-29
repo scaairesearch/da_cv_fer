aaba8d40818de48f246422b1f0bddc2a32b1c36e
scaairesearch <157268702+scaairesearch@users.noreply.github.com>
2024-01-29 16:56:06 +0530

    included patience and changed starting lr

--- run_config.py
+++ run_config.py
@@ -6,24 +6,25 @@ import os
 
 class RunConfig:
     def __init__(self) -> None:
-        self.EPOCHS = 1
-        self.NUM_EPOCHS = 1
+        self.EPOCHS = 2
+        self.NUM_EPOCHS = 2
         self.IN_COLAB = False
         if os.path.exists('/content'): # check for google colab
             self.IN_COLAB = True
         
         if self.IN_COLAB:
-            self.EPOCHS = 100
-            self.NUM_EPOCHS = 100  
+            self.EPOCHS = 50
+            self.NUM_EPOCHS = 50  
         # self.optimizer = None
         self.lr_strategy = None
-        self.inital_lr = 0.005
+        self.inital_lr = 0.001
         self.use_cuda = torch.cuda.is_available()
         self.device = torch.device("cuda" if self.use_cuda else "cpu")
         self.criterion_class = nn.CrossEntropyLoss() # we are not doing log_softmax or softmax
         self.criterion_domain = nn.BCEWithLogitsLoss() # we can also use nn.CrossEntropyLoss() - we are not doing log_softmax or softmax
 
-
+        self.lr_patience = 3
+        self.early_stop_patience = 5
         self.EWC_LAMBDA = 0.4
         
 
