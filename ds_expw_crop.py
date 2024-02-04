8514a3b289edf4d8ba80203186be83a651e426d0
scaairesearch <157268702+scaairesearch@users.noreply.github.com>
2024-02-04 16:03:52 +0530

    fix with MTCNN

--- ds_expw_crop.py
+++ ds_expw_crop.py
@@ -28,12 +28,10 @@ class DatasetEXPWCROP(Dataset):
         self.crop_at_runtime = crop_at_runtime
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         if self.crop_at_runtime:
-            self.mtcnn = MTCNN(image_size=224).to(device=self.device)
+            self.mtcnn = MTCNN(image_size=224, device= self.device)#MTCNN(image_size=224).to(device=self.device)
         else:
-            self.mtcnn = MTCNN(image_size=224).to(device=self.device)#.to(device='cpu') # always wanted on CPU
-
-
-
+            # self.mtcnn = MTCNN(image_size=224, device= self.device)#MTCNN(image_size=224).to(device=self.device)#.to(device='cpu') # always wanted on CPU
+            self.mtcnn = MTCNN(image_size=224).to(device='cpu') # always wanted on CPU
 
 
         dataconfig = DataConfig()
