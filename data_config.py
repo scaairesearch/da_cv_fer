3a7865494ed5684d92e380e7261281ad8f158456
scaairesearch <157268702+scaairesearch@users.noreply.github.com>
2024-05-24 05:42:01 +0530

    Changing EXPW Train images to 2000

--- data_config.py
+++ data_config.py
@@ -84,7 +84,7 @@ class DataConfig:
         self.EXPW_PARTIAL = 0.005 # Fraction of dataset to be used
         self.EXPW_TRAIN_TEST_SPLIT = 0.95
         self.EXPW_VAL_DECISION = 'race' # 'race' or 'partial'
-        self.EXPW_TRAINING_SAMPLES = 1000
+        self.EXPW_TRAINING_SAMPLES = 2000 #1000
         self.EXPW_mean_ds = [0.3917, 0.3120, 0.2759]
         self.EXPW_std_dev_ds = [0.2205, 0.2134, 0.2277]
         if self.IN_COLAB:
